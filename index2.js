b28da1538193c4942be2946ef20ee91b89f456c2
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-25 04:57:11 -0700

    jsno1 to no3

---
+++ index2.js
@@ -0,0 +1,10 @@
+var fname;
+var lname;
+var age;
+var dob;
+var address;
+fname = 'Ahmed';
+lname = 'Satti'
+age = 35;
+dob = '12-10-1986'
+address = 'calgary, AB'
---
+++ index3.js
@@ -0,0 +1,6 @@
+var fname, lname, age, dob, address;
+fname = 'Ahmed';
+lname = 'Satti'
+age = 35;
+dob = '12-10-1986'
+address = 'calgary, AB'
