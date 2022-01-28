e0c633e2e3add72189abd614da8c5e8dda193ae9
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-28 10:17:45 -0700

    No 29

---
+++ index29.js
@@ -0,0 +1,3 @@
+let length = 10;
+let perimeter = length * 4;
+console.log('When side length is ' + length +  ' ' + 'the perimeter of a square is' + ' ' + perimeter + '.' + ' Perimeter = 4 x 10 = 40');
