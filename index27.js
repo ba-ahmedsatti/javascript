c91e84e70dee617845bf8538b61ecadcc590a0d5
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-28 08:12:21 -0700

    No 27

---
+++ index27.js
@@ -0,0 +1,21 @@
+let firstNumber = 10;
+let secondNumber = 20;
+
+console.log(firstNumber + secondNumber);
+console.log(firstNumber / secondNumber);
+console.log(secondNumber / firstNumber);
+console.log(firstNumber * secondNumber);
+console.log(firstNumber - secondNumber);
+console.log(secondNumber - firstNumber);
+console.log(firstNumber % secondNumber);
+console.log(secondNumber % firstNumber);
+
+let num1 = 2;
+let num2 = 3;
+let result = num1 + num2;
+let result1 = num1 / num2;
+let result2 = num1 % num2;
+let result3 = num1 - num2;
+let result4 = num1 * num2;
+console.log(result, result1, result2, result3, result4);
+
