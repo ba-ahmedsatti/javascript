baa6476b07089e73cb6c1a2f09e9dbf7e3d9074f
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-28 11:06:16 -0700

    No 31

--- index30.js
+++ index30.js
@@ -1,4 +1,4 @@
 let length = 5;
 let width = 5;
-let area = (length * 2) + (width * 2);
-console.log('When side length is ' + length +  ' ' + 'the width of a square is' + ' ' + width + '.' + ' Area = 4 x 5 =' + ' ' + area);
+let area = length * width;
+console.log('When side length is ' + length +  ' ' + 'the width of a square is' + ' ' + width + '.' + ' Area = 5 x 5 =' + ' ' + area);
---
+++ index31.js
@@ -0,0 +1,5 @@
+let firstSide = 10;
+let secondSide = 20;
+let thirdSide = 5;
+let perimeter = firstSide + secondSide + thirdSide;
+console.log('When first side is ' + firstSide + ' and the second side is' + ' ' + secondSide + ', and the third side is' + ' ' + thirdSide +' then Perimeter = 10 + 20 + 5 = ' + perimeter);
