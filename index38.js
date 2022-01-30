f5dbd89d50c0a7ed18f7d3939041c7e081c975db
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-30 06:14:45 -0700

    Index38

---
+++ index38.js
@@ -0,0 +1,4 @@
+let firstName = 'Ahmed';
+console.log('My name is ' + firstName + ' and it is ' + firstName.length + ' characters long');
+console.log(`My name is ${firstName} and it is ${firstName.length} characters long`);
+
