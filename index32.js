9a655b7f794646b6a771999a0c71c1a8017057c3
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-30 03:40:44 -0700

    No 32

---
+++ index32.js
@@ -0,0 +1,8 @@
+let height = 10;
+let base = 4;
+let rectangularPerimiter = 2 *(height + base);
+let rectangularArea = height * base;
+console.log('To calculate Rectangular Perimiter, When height is ' + height + ' and width (base) is ' + base + ', Rectangular Perimeter = 2(heigh + width) = ' + rectangularPerimiter);
+console.log('To calculate Rectangular Area, When height is ' + height + ' and width (base) is ' + base + ', Rectangular Perimeter =  ' + rectangularArea);
+console.log('Perimiter of Rectangular: ', rectangularPerimiter);
+console.log('Area of Rectangular: ', rectangularArea);
