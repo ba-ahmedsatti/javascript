ebff61e57079b44b02f9069fd9da2e114c75d041
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-30 04:41:28 -0700

    No 33

---
+++ index33.js
@@ -0,0 +1,11 @@
+let userCount = 100;
+userCount += 5;
+console.log(`Users count: ${userCount}`);
+console.log('Users count: '+userCount);
+userCount -= 3;
+console.log('Users count: ', userCount);
+userCount *= 2;
+console.log('Users count: ', userCount);
+menCount = (50/100) * userCount
+womenCount = (50/100) * userCount
+console.log('Users count: ', userCount + ' ' + menCount + ' & ' + womenCount);
