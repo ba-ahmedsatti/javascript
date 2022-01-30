f81096b819831a43a310ac332130cc45c24a5b21
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-30 06:40:59 -0700

    Index39

---
+++ index39.js
@@ -0,0 +1,13 @@
+let firstName ='Ahmed';
+let lastName ='Satti'
+console.log(`My first name is ${firstName} and it is ${firstName.length} characters long`);
+console.log(`My last name is ${lastName} and it is ${lastName.length} characters long`);
+console.log(`The character difference between my first name and last name is ${firstName.length - lastName.length}`);
+console.log(`My first name is longer than my last name: ${firstName.length > lastName.length}`);
+
+let heroFirstName ='Michael';
+let heroLastName ='Jordan'
+console.log(`My first name is ${heroFirstName} and it is ${heroFirstName.length} characters long`);
+console.log(`My last name is ${heroLastName} and it is ${heroLastName.length} characters long`);
+console.log(`The character difference between my first name and last name is ${heroFirstName.length - heroLastName.length}`);
+console.log(`My first name is longer than my last name: ${heroFirstName.length > heroLastName.length}`);
