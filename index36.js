9de25328228988b00d87c2b4057e4ec0b7cbfa6d
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-30 05:46:16 -0700

    No 36

---
+++ index36.js
@@ -0,0 +1,4 @@
+let firstNumber = 10;
+let secondNumber = 20;
+console.log('Both varibles have the same value: ', firstNumber == secondNumber);
+console.log('Both varibales don\'t have the same value: ', firstNumber != secondNumber);
