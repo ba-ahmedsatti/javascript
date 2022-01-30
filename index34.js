f33fce7db023da7c4c1df13ca126960ce47fbbd7
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-30 05:06:34 -0700

    No 34

---
+++ index34.js
@@ -0,0 +1,6 @@
+let income = 1000;
+let revenue = 600;
+let taxes = 500;
+console.log('Income Objective: ', income >= 800);
+console.log('Spend on Taxes Objective: ', taxes < 400);
+console.log('Bonus Target: ', revenue >= 600);
