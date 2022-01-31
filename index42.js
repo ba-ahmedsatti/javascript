c019c8e60b2ff2000d4665f7c0f5c175c12e4c33
ba-ahmedsatti <aihsatti@gmail.com>
2022-01-31 08:44:31 -0700

    index42

---
+++ index42.js
@@ -0,0 +1,11 @@
+let message = 'Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopian 2044, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game,\n the discovery of which will lead him to inherit the game creator\'s fortune. Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House)]. The book was published on August 16, 2011. An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association and won the 2012 Prometheus Award.';
+console.log('Upper Case: ', message.toUpperCase());
+console.log('Lower Case: ', message.toLowerCase());
+
+let transform = 'Ready Player One is a 2011 science fiction novel,  \n  and the debut novel of American author Ernest Cline. The story, set in a dystopian 2044, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game,\n the discovery of which will lead him to inherit the game creator\'s fortune. ';
+console.log('Transform: ', transform.toUpperCase());
+
+
+let transformLower = 'Cline sold the rights to publish the novel in June 2010, \n in a bidding war to the Crown Publishing Group (a division of Random House)].\n The book was published on August 16, 2011. An audiobook was released the same day; it was narrated by Wil Wheaton,\n who was mentioned briefly in one of the chapters.Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association and won the 2012 Prometheus Award.';
+console.log(transformLower.toLowerCase());
+
