d45fa5f8e446df13c9fec0cf7698d3217809cb49
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-03 04:15:29 -0700

    Index44

---
+++ index44.js
@@ -0,0 +1,19 @@
+// Exercise 44
+// Create a new index44.js file
+// Define a text variable and assign the following value: HELLO
+// Define a result variable
+// Get each char from the text variable and revert the text assigning it to the result variable
+// At the end you should be able to show the following text: OLLEH
+// Once you reverted the text, transform it to lowercase
+// Show the final output: the final result is: olleh
+// It kind of sound spanish, right?.. ole!!! (lol)
+
+let text ='HELLO';
+let result = text.slice(2);
+
+console.log (result);
+
+const text = 'JavaScript and Java are not the same';
+const result1 = text.slice(0, -20); // JavaScript
+
+console.log(result1);
