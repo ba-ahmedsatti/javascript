c98b0dc880d8e4c4354f3f4c412fba67b302da49
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-03 04:27:18 -0700

    testing loops

---
+++ test.js
@@ -0,0 +1,19 @@
+// // while (false) {
+// //     console.log('This code doesn\'t get executed');
+// //   }
+
+//   let number = 1000;
+
+//   while (number < 10) {
+//     console.log('number: ', number);
+//     number++;
+//     // All this code won't get executed as the initial condition is false
+//   }
+
+
+let number = 0;
+
+while (number < 100) {
+  console.log('number: ', number);
+  number ++;
+}
