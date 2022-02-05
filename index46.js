3672c99828f3a37e27fff86e33e043f5cc966653
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-05 08:49:45 -0700

    Index46

---
+++ index46.js
@@ -0,0 +1,10 @@
+let userAndPassword = 'pepito2017,12345';
+let username = userAndPassword.substring(0, 10);
+let password = userAndPassword.substring(11, 16);
+
+console.log(username);
+console.log(password);
+
+console.log(`The user ${username} has ${password} as password`);
+
+
