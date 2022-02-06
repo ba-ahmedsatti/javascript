526cb0d96432098fd5147763f55d00374136a3e9
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-06 08:52:47 -0700

    Index55 -  conditional ternary operator

---
+++ index55.js
@@ -0,0 +1,23 @@
+// Exercise 55
+// Create a new index55.js file
+// Copy and paste the code from exercise 52
+// Refactor the code so it match the new requirements:*
+// Replace if/ese statement for a conditional ternary operator
+
+
+let firstNumber = 5;
+let secondNumber = 10;
+
+let conditionalTernaryOperator = (firstNumber > secondNumber) ? console.log(`Number ${firstNumber} is bigger than ${secondNumber}`):console.log(`Number ${secondNumber} is bigger than ${firstNumber}`);
+
+let number = 2;
+let message = (number === 2) ? console.log('The number is 2') : console.log('The number is not 2');
+
+// if (firstNumber > secondNumber) 
+// {
+// console.log(`Number ${firstNumber} is bigger than ${secondNumber}`);
+// }
+// else
+// {
+//     console.log(`Number ${secondNumber} is bigger than ${firstNumber}`);
+//     }
