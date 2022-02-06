f76bee905603e16693f20b8714e09ab724bb8a2b
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-06 09:00:11 -0700

    Index56 conditional ternary operator if/else

---
+++ index56.js
@@ -0,0 +1,18 @@
+// Exercise 56
+// Create a new index56.js file
+// Copy and paste the code from exercise 53
+// Refactor the code so it match the new requirements:*
+// Replace if/ese statement for a conditional ternary operator
+
+const number = 11;
+
+const conditionalTernaryOperator = (number === 10) ? console.log(`${number} is an even number`) : console.log(`${number} is an odd number`);
+
+
+// if (number === 10)
+// {
+//     console.log(`${number} is an even number`);
+// } else
+// {
+//     console.log(`${number} is an odd number`);
+// }
