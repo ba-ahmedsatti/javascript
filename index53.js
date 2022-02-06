2c1ffffe5582aa1dbe41a67ad06394ddb507413d
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-06 08:23:19 -0700

    Index53

---
+++ index53.js
@@ -0,0 +1,24 @@
+// Exercise 53
+// Create a new index53.js file
+// Copy and paste the code from exercise 50
+// Refactor the code so it match the new requirements:
+// If number is not even then show the following message: %number% is an odd number
+
+
+
+
+const number = 11;
+if (number === 10)
+{
+    console.log(`${number} is an even number`);
+} else
+{
+    console.log(`${number} is an odd number`);
+}
+
+// for(let count =0; count<=100; count++)
+// {
+//     (count %2 === 0) ?
+//     console.log(`${count} is even`):console.log(`${count} is odd`);
+    
+// }
