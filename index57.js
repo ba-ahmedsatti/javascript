5eb8d0b3b471c4e89c9dde313d3c3f15aeb0e530
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-07 02:25:32 -0700

    Index57 conditional ternary operator

---
+++ index57.js
@@ -0,0 +1,18 @@
+// Exercise 57
+// Create a new index57.js file
+// Copy and paste the code from exercise 54
+// Refactor the code so it match the new requirements:*
+// Replace if/ese statement for a conditional ternary operator
+
+const username = 'pepe2017';
+const password = '12345';
+
+const Refactor = (username === 'pepe2017' && password === '12345') ? console.log('Logged in user, show user home page') : console.log('Sorry, there has been a problem, please try it again.');
+
+// if (username === 'pepe2017' && password === '12344')
+// {
+//     console.log('Logged in user, show user home page');
+//     }else
+//     {
+//         console.log('Sorry, there has been a problem, please try it again.');
+//     }
