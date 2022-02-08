516d31bdcab852dde04881b7bd60ded6fc8fb9a4
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-08 06:34:00 -0700

    update If statement Even & Odd numbers

--- index53.js
+++ index53.js
@@ -4,17 +4,26 @@
 // Refactor the code so it match the new requirements:
 // If number is not even then show the following message: %number% is an odd number
 
-
-
-
 const number = 11;
-if (number === 10)
+if (number%2 === 0)
 {
     console.log(`${number} is an even number`);
 } else
 {
     console.log(`${number} is an odd number`);
 }
+
+// 1. example:
+
+// let number=10;
+// if(number%2==0){
+//     console.log(number+" is an even number")
+// }
+// else{
+//     console.log(number+" is an odd number")
+// }
+
+// 2. example:
 
 // for(let count =0; count<=100; count++)
 // {
