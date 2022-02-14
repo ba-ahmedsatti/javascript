367c61dabb32d4f087c0974f6e1463a6037676a0
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-14 06:18:35 -0700

    index88 for loop--

---
+++ index88.js
@@ -0,0 +1,16 @@
+// Exercise 87
+// Create a new index87.js file
+// Copy and paste the code from exercise 77
+// Refactor the code to use for instead of do/while
+
+// let number = 100;
+// do 
+// {
+//     console.log(number);
+//     number--;
+// }while(number > 0);
+
+for(let number =100; number > 0; number--)
+{
+    console.log(number);
+}
