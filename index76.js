fbd19cc2b7ea396cc70e6dcd59dc78691c9a742e
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-14 05:06:19 -0700

    Index76 do/while loop

---
+++ index76.js
@@ -0,0 +1,13 @@
+// Exercise 76
+// Create a new index76.js file
+// Copy and paste the code from exercise 66
+// Refactor the code to use do/while instead of while
+
+
+let number = 0;
+do
+{
+    console.log(number);
+    number++;
+}
+while(number < 11);
