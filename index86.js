dbe5e70da695a9ac2dba110e2c016b7449aef06e
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-14 06:12:51 -0700

    index86 for loop

---
+++ index86.js
@@ -0,0 +1,17 @@
+// Exercise 86
+// Create a new index86.js file
+// Copy and paste the code from exercise 76
+// Refactor the code to use for instead of do/while
+
+// let number = 0;
+// do
+// {
+//     console.log(number);
+//     number++;
+// }
+// while(number < 11);
+
+for(let number = 0; number < 11; number++)
+{
+    console.log(number);
+}
