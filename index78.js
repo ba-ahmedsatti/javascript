27ede5043684923791ce8bc78e274c30f04d4df0
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-14 05:15:54 -0700

    Index78 do/while loo[ even numbers

---
+++ index78.js
@@ -0,0 +1,20 @@
+// Exercise 78
+// Create a new index78.js file
+// Copy and paste the code from exercise 68
+// Refactor the code to use do/while instead of while
+
+// let number = 0;
+
+// while(number <= 100)
+// {
+//     console.log(number);
+//     number += 2;
+// }
+
+let number = 0;
+do
+{
+    console.log(number);
+    number +=2;
+    }while(number <=100);
+
