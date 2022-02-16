9ec9f25fd7b53eef4e300335cb01903c40d9c778
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-16 04:43:38 -0700

    index72 while loop stars increment

---
+++ index72.js
@@ -0,0 +1,27 @@
+// Exercise 72
+// Create a new index72.js file
+// Show the following output using while:
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+// ***********
+// ************
+// *************
+
+
+let i = 1;
+let stars = '';
+
+while(i<14)
+{
+    stars = stars + '*';
+    console.log(stars);
+    i++;
+}
