4c94c34a69ab4347ae133debf87c907ea5a6432d
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-17 05:28:15 -0700

    index82 do/while loop

---
+++ index82.js
@@ -0,0 +1,32 @@
+// Exercise 82
+// Create a new index82.js file
+// Copy and paste the code from exercise 72
+// Refactor the code to use do/while instead of while
+
+
+// let i = 1;
+// let stars = '';
+
+// while(i<14)
+// {
+//     stars = stars + '*';
+//     console.log(stars);
+//     i++;
+// }
+
+let i = 1;
+let stars = '';
+
+do
+{
+    stars = stars + '*';
+    console.log(stars);
+    i++;
+}while(i < 14);
+
+// let number = 0;
+// do
+// {
+//     console.log(number);
+//     number +=2;
+//     }while(number <=100);
